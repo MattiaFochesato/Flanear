97abdfdf16bceba3c817546f25b1937d571a1a0e
Mattia Fochesato <36085165+MattiaFochesato@users.noreply.github.com>
2022-02-11 15:52:50 +0100

    Initial Commit

---
+++ Flanear/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Flanear
+//
+//  Created by Mattia Fochesato on 11/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Flanear/FlanearApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlanearApp.swift
+//  Flanear
+//
+//  Created by Mattia Fochesato on 11/02/22.
+//
+
+import SwiftUI
+
+@main
+struct FlanearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
