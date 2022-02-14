11531d700e01b458579aed59aba4c3c1a3e644d5
Mattia Fochesato <36085165+MattiaFochesato@users.noreply.github.com>
2022-02-14 18:08:16 +0100

    Added extension

---
+++ Flanear/Extensions/CLLocationCoordinate2D+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  CLLocationCoordinate2D+Extension.swift
+//  Flanear
+//
+//  Created by Mattia Fochesato on 14/02/22.
+//
+
+import Foundation
+import CoreLocation
+
+extension CLLocationCoordinate2D: Hashable {
+    public static func == (lhs: Self, rhs: Self) -> Bool {
+        return lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude
+    }
+    
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(latitude)
+        hasher.combine(longitude)
+    }
+}
