0d81e3dbec879b7cb84aeee79b15df5d78f09e8f
Mattia Fochesato <36085165+MattiaFochesato@users.noreply.github.com>
2022-02-25 11:38:06 +0100

    Added CameraView

---
+++ Flanear/Views/Diary/Camera/CameraView.swift
@@ -0,0 +1,20 @@
+//
+//  CameraView.swift
+//  Flanear
+//
+//  Created by Mattia Fochesato on 25/02/22.
+//
+
+import SwiftUI
+
+struct CameraView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CameraView_Previews: PreviewProvider {
+    static var previews: some View {
+        CameraView()
+    }
+}
